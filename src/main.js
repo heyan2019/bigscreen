d90b3b579510d292ebe439bed5bb38a5249038b9
yanhe <yandewanghe@126.com>
2019-09-05 18:30:03 +0800

    测试完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts //将echarts注册成Vue的全局属性
 Vue.config.productionTip = false
 
 new Vue({
